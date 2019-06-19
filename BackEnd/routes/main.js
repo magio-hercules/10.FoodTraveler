536abbb639a611f6180e09b0e3fedccf4cd5ea49
hercules <kjmhercules@gmail.com>
2019-06-19 14:00:54 +0900

    [BackEnd] 갤러리 DB 조회 기능 추가

--- BackEnd/routes/main.js
+++ BackEnd/routes/main.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-
 // controller
 var controller_contents = require('../controllers/contents');
-var controller_food 	= require('../controllers/foods');
-var controller_store 	= require('../controllers/stores');
-var controller_info 	= require('../controllers/information');
-
+var controller_food = require('../controllers/foods');
+var controller_store = require('../controllers/stores');
+var controller_info = require('../controllers/information');
 
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
@@ -23,38 +21,33 @@ router.get('/', function(req, res) {
 	res.send('Welcome to FoodTraveler');
 });
 
-
 /////////
 // GET //
 /////////
 
 // food //
-router.get('/total_foods', 				controller_food.getTotalFoods);
+router.get('/total_foods', controller_food.getTotalFoods);
 
 // store //
-router.get('/total_stores', 			controller_store.getTotalStores);
-
-
-
+router.get('/total_stores', controller_store.getTotalStores);
 
 //////////
 // POST //
 //////////
 
 // food //
-router.post('/food',					controller_food.postFood);
-router.post('/description',				controller_food.postDescription);
+router.post('/food', controller_food.postFood);
+router.post('/description', controller_food.postDescription);
 
 // store //
-router.post('/store',					controller_store.postStore);
-
+router.post('/store', controller_store.postStore);
+router.post('/gallery', controller_store.postGallery);
 
 // info //
-router.post('/ingredient',				controller_info.postIngredient);
-router.post('/cook',					controller_info.postCook);
-router.post('/eat',						controller_info.postEat);
-router.post('/history',					controller_info.postHistory);
-router.post('/caution',					controller_info.postCaution);
-
+router.post('/ingredient', controller_info.postIngredient);
+router.post('/cook', controller_info.postCook);
+router.post('/eat', controller_info.postEat);
+router.post('/history', controller_info.postHistory);
+router.post('/caution', controller_info.postCaution);
 
 module.exports = router;
