bcc7033a8abf20f8c032473b34dd8cdc65e01b69
hercules <kjmhercules@gmail.com>
2019-07-15 14:34:41 +0900

    [Android] package명 변경 (facebook로그인 때문)

--- ReactNative/android/app/src/main/java/com/foodtraveler/MainActivity.java
+++ ReactNative/android/app/src/main/java/com/foodtraveler/MainActivity.java
@@ -1,4 +1,4 @@
-package com.foodtraveler;
+package com.labis.foodtraveler;
 
 import android.os.Bundle; // here
 import com.facebook.react.ReactActivity;
--- ReactNative/android/app/src/main/java/com/foodtraveler/MainApplication.java
+++ ReactNative/android/app/src/main/java/com/foodtraveler/MainApplication.java
@@ -1,4 +1,4 @@
-package com.foodtraveler;
+package com.labis.foodtraveler;
 
 import java.util.Arrays;
 import java.util.List;
